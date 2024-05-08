eeeb9074a27b6835cd167658d303344299ca13b7
Italo Git <sulamita.italo12@gmail.com>
2024-05-08 14:32:56 -0300

    reajuste

--- Estrutura/script.js
+++ Estrutura/script.js
@@ -1,5 +1,3 @@
-function gerar() {
-    let parc = document.getElementById('parc')
-    let parcela = document.getElementById('parcela')
-    parc.innerHTML = parcela.value
+function pagar() {
+    window.alert('## PAGAMENTO REALIZADO COM SUCESSO! ##')
 }
