4dbce3dd17311818dd7056e75838ee9a9cb6ae63
rigelsss <rigelsouza@gmail.com>
2024-05-20 08:47:59 -0300

    melhorias
    código html atualizado para correta exibição
    código js atualizado
    
    no aguardo da definição do estilo CSS para implementação
    no aguardo da definição do header e footer

--- simulacao/js.js
+++ simulacao/js.js
@@ -24,18 +24,16 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function addItem(value1, value2) {
-        console.log('Adicionando item:', { value1, value2 });
-
-        const li = document.createElement('li');    
-        li.innerHTML = `
-        Valor da compra nesta simulação: ${value1} reais<br>
-        Quantidade de parcelas nesta simulação: ${value2} parcelas<br>
-        Seu desconto nessa compra é de ${(value1*0.001)} centavos<br>
-        Você está acumulando ${(14/100)} pontos no seu plano MyStudentcadr<br>
-        Seu cashback nessa compra é de ${(value1 * 0.35).toFixed(2)} centavos<br>
-        
-
-    `;
+  
+            itemList.innerHTML = '';
+            const li = document.createElement('li');    
+            li.innerHTML = `
+            Valor da compra nesta simulação: ${value1} reais<br>
+            Quantidade de parcelas nesta simulação: ${value2} parcelas<br>
+            Seu desconto nessa compra será de ${(value1*0.001)} centavos<br>
+            Você está acumulando ${(14/100)} pontos no seu plano MyStudentcard<br>
+            Seu cashback nessa compra será de ${(value1 * 0.035).toFixed(2)} centavos<br>`
+    ;
 
         itemList.appendChild(li);            
     }
